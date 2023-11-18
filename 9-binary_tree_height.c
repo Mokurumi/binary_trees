4eab68c3f96c293fe507e6604230440864264826
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-11-18 03:47:31 -0800

    binary tree height correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,5 +19,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return ((h_left > h_right) ? h_left : h_left);
+	return ((h_left > h_right) ? h_left : h_right);
 }
