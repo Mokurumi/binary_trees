99bc9315140f1517506490ca2540a93bf8f19aed
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-11-19 01:57:10 -0800

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,14 +47,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	size_t height = binary_tree_height(tree);
 	size_t size = binary_tree_size(tree);
 
-	size_t perfect_nodes = 1;
-
-	while (height > 0)
-	{
-		perfect_nodes *= 2;
-		height--;
-	}
-
-	/* Check if the tree is perfect */
-	return (size == perfect_nodes);
+	return (binary_tree_is_full(tree) && (size == (1 << height) - 1));
 }
