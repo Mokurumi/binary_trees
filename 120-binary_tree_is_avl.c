aef329b7c4c17f63e59a1aa479ba8ce8e0ed8bb6
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-12-18 04:43:24 +0300

    Update 120-binary_tree_is_avl.c

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,10 +1,17 @@
 #include "binary_trees.h"
+#include "14-binary_tree_balance.c"
 
 /**
  * binary_tree_is_avl - Checks if a binary tree is a valid AVL Tree
  * @tree: Pointer to the root node of the tree to check
  *
  * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ * If tree is NULL, return 0
+ * One value in right subtree is lower than root node value, return 0
+ * One value in left subtree is higher than root node value, return 0
+ * Duplicate values are not allowed in this AVL Tree
+ * Single node tree, return 1
+ * Perfect BST of any height, return 1
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
@@ -18,47 +25,19 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
+	{
 		if (tree->left->n > tree->n)
 			return (0);
+	}
 
 	if (tree->right)
+	{
 		if (tree->right->n < tree->n)
 			return (0);
+	}
 
-	if (tree->left && tree->right)
-		if (tree->left->n > tree->right->n)
-			return (0);
-
-	if (tree->left)
-		if (!binary_tree_is_avl(tree->left))
-			return (0);
-
-	if (tree->right)
-		if (!binary_tree_is_avl(tree->right))
-			return (0);
-
-	return (1);
-}
-
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
- *
- * Return: The balance factor of the tree, or 0 if tree is NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int left = 0;
-	int right = 0;
-
-	if (!tree)
+	if (!binary_tree_is_avl(tree->left) || !binary_tree_is_avl(tree->right))
 		return (0);
 
-	if (tree->left)
-		left = 1 + binary_tree_balance(tree->left);
-
-	if (tree->right)
-		right = 1 + binary_tree_balance(tree->right);
-
-	return (left - right);
+	return (1);
 }
