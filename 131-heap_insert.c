9dbaa1bd25f0a96966d66debd0f8f98e53509fdf
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-12-18 05:40:26 +0300

    Update 131-heap_insert.c

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -94,26 +94,3 @@ size_t binary_tree_size(const heap_t *tree)
 
 	return (size);
 }
-
-/**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: value to store in the new node
- *
- * Return: pointer to the new node, or NULL on failure
- */
-heap_t *binary_tree_node(heap_t *parent, int value)
-{
-	heap_t *new_node = NULL;
-
-	new_node = malloc(sizeof(heap_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	return (new_node);
-}
