a210d7df115c9d8463e7acad85b6ee18d9bf5ca3
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-12-18 05:36:44 +0300

    Update 131-heap_insert.c

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,4 +1,7 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+#include "0-binary_tree_node.c"
+#include "11-binary_tree_size.c"
 
 /**
  * heap_insert - inserts a value into a Max Binary Heap
@@ -9,43 +12,69 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node, *parent;
+	heap_t *new_node = NULL;
 
 	if (!root)
 		return (NULL);
 
-	parent = *root;
-	new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(NULL, value);
 	if (!new_node)
 		return (NULL);
 
-	if (!parent)
+	if (!*root)
 	{
 		*root = new_node;
 		return (new_node);
 	}
 
-	while (1)
-	{
-		if (parent->n >= value)
-		{
-			if (!parent->left)
-			{
-				parent->left = new_node;
-				break;
-			}
-			parent = parent->left;
-		}
-		else
-		{
-			if (!parent->right)
-			{
-				parent->right = new_node;
-				break;
-			}
-			parent = parent->right;
-		}
-	}
+	insert_node(*root, new_node);
+	swap_nodes(new_node);
 
 	return (new_node);
+}
+
+/**
+ * insert_node - inserts a node into a Max Binary Heap
+ * @root: pointer to the root node of the Heap
+ * @node: pointer to the node to be inserted
+ */
+void insert_node(heap_t *root, heap_t *node)
+{
+	if (root->left && root->right)
+	{
+		if (binary_tree_size(root->left) <= binary_tree_size(root->right))
+			insert_node(root->left, node);
+		else
+			insert_node(root->right, node);
+	}
+	else
+	{
+		if (!root->left)
+			root->left = node;
+		else
+			root->right = node;
+		node->parent = root;
+	}
+}
+
+/**
+ * swap_nodes - swaps a node with its parent
+ * @node: pointer to the node to be swapped
+ */
+void swap_nodes(heap_t *node)
+{
+	int temp;
+
+	while (node->parent)
+	{
+		if (node->n > node->parent->n)
+		{
+			temp = node->n;
+			node->n = node->parent->n;
+			node->parent->n = temp;
+			node = node->parent;
+		}
+		else
+			break;
+	}
 }
